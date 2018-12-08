441062b901843d64f695bd85da8e3a44533c26b8
Kelvin Tan <kelvin.vins@gmail.com>
2018-12-08 10:21:44 +0800

    first commit

--- SearchViewController/ViewController.swift
+++ SearchViewController/ViewController.swift
@@ -10,11 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    var data = ["Apple","Microsoft","Twitter","Facebook","Instagram"]
+    var filteredData = [String]()
+    var isSearching = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        searchBar.delegate = self
+        tableView.dataSource = self
+        searchBar.enablesReturnKeyAutomatically = false
+        tableView.keyboardDismissMode = .onDrag
     }
+}
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if isSearching {
+            return filteredData.count
+        } else {
+            return data.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        if isSearching {
+            cell.textLabel?.text = filteredData[indexPath.row]
+        } else {
+            cell.textLabel?.text = data[indexPath.row]
+        }
+        return cell
+    }
+}
 
+extension ViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text == "" {
+            isSearching = false
+            tableView.reloadData()
+        } else {
+            isSearching = true
+            filteredData = data.filter({$0.contains(searchBar.text ?? "")})
+            tableView.reloadData()
+        }
+    }
 }
 
